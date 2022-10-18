9f656916da0bcc08c6656806b335c6c813b71ceb
moh2001alanzi <113971829+moh2001alanzi@users.noreply.github.com>
2022-10-18 09:55:43 +0300

    Create hellodart.dart
    First file

---
+++ hellodart.dart
@@ -0,0 +1,17 @@
+void main() 
+{
+ print('hello worid');
+ String nk = "hello class students";
+ print(nk); 
+  
+  bool x;
+  x = 1000>200;
+  print(x);
+  
+  String name = "Shaqra";
+  if (name.length!=0)   {
+   print("String not empty!");
+ } else { 
+   print("String is empty!");
+ } 
+}
